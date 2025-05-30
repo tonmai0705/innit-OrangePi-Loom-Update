8e3f14eafb3e0b2a25b60bcd18f27d71120ecc70
PK. <70423191+tonmai0705@users.noreply.github.com>
2025-05-30 14:20:46 +0700

    Create update-to-loom.sh

---
+++ update-to-loom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "กำลัสร้างไฟล์...gitDLTest.sh"
+cat << EOR > gitDLTest.sh
+#!/bin/bash
+echo "ปริ้นนนนนนนนนนนนนเด้อ"
+
+EOR
+echo "กำลัสร้างไฟล์  gitDLTest.sh is successfully"
+chmod +x gitDLtest.sh
+
+echo "กำลัสร้างไฟล์...gitDLTest2.sh"
+cat << EOR > gitDLTest2.sh
+#!/bin/bash
+echo "ปริ้นนนนนนนนนนนนนเด้อ2"
+
+EOR
+echo "กำลัสร้างไฟล์  gitDLTest2.sh is successfully"
+chmod +x gitDLtest2.sh
