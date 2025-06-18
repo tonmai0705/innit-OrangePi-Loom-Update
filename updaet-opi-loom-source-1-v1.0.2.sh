654856bccf90f8b46fbcb35e58f758dff1788388
PK. <70423191+tonmai0705@users.noreply.github.com>
2025-06-18 09:49:50 +0700

    Update updaet-opi-loom-source-1-v1.0.2.sh

--- updaet-opi-loom-source-1-v1.0.2.sh
+++ updaet-opi-loom-source-1-v1.0.2.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo " [ACK-IoT Orange Pi Update Version] ลบไฟล์ flow.json ลบไฟล์..."
-echo " "
-echo " [ACK-IoT Orange Pi Update Version] Update Node-red-flows.json is Successful... Please Restart node-red By CMD --> node-red-restart"
+echo " [ACK-IoT Orange Pi Update Version] Successful "
