9f974dcfda5482019538b86c12fe448710c6a1a4
PK. <70423191+tonmai0705@users.noreply.github.com>
2025-06-23 10:28:42 +0700

    Update findfile-opi.sh

--- findfile-opi.sh
+++ findfile-opi.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-user="$HOME"
+user=$HOME
 
-if [ -f "user/loom/config.txt" ]; then
+if [ -f "$user/loom/config.txt" ]; then
         echo "พบไฟล์"
 else
         echo "ไม่พบไฟล์"
 fi
 
-if [ -d "user/loom" ]; then
+if [ -d "$user/loom" ]; then
         echo "พบไดเลกทอรี่ loom"
 else
 echo "ไม่พบไดเลกทอรี่ loom"
 fi
 
-if [ -d "user/loo1m" ]; then
+if [ -d "$user/loo1m" ]; then
         echo "พบไดเลกทอรี่ loo1m"
 else
 echo "ไม่พบไดเลกทอรี่ loo1m"
