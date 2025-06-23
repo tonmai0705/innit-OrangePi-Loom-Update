760d104a2ecc77fcefe541a61d542336f9a20a70
PK. <70423191+tonmai0705@users.noreply.github.com>
2025-06-23 10:23:43 +0700

    Create findfile-opi.sh

---
+++ findfile-opi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+user=$HOME
+
+if [ -f "user/loom/config.txt" ]; then
+        echo "พบไฟล์"
+else
+        echo "ไม่พบไฟล์"
+fi
+
+if [ -d "user/loom" ]; then
+        echo "พบไดเลกทอรี่ loom"
+else
+echo "ไม่พบไดเลกทอรี่ loom"
+fi
+
+if [ -d "user/loo1m" ]; then
+        echo "พบไดเลกทอรี่ loo1m"
+else
+echo "ไม่พบไดเลกทอรี่ loo1m"
+fi
