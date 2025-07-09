f7eca414380e2514cf1d92937d17caf30516f260
PK. <70423191+tonmai0705@users.noreply.github.com>
2025-07-09 09:54:09 +0700

    Update edit-sudoers.d.sh

--- edit-sudoers.d.sh
+++ edit-sudoers.d.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo 'orangepi ALL=(ALL) NOPASSWD: /bin/systemctl restart nodered.service' | sudo tee /etc/sudoers.d/nodered-nopasswd
+echo 'orangepi ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart nodered.service' | sudo tee /etc/sudoers.d/nodered-nopasswd
 sudo chmod 440 /etc/sudoers.d/nodered-nopasswd
