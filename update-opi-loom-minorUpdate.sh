281c6047b53a35e1b67e871dd2cbf80bcc48cb9a
PK. <70423191+tonmai0705@users.noreply.github.com>
2025-07-14 14:12:06 +0700

    Update update-opi-loom-minorUpdate.sh

--- update-opi-loom-minorUpdate.sh
+++ update-opi-loom-minorUpdate.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+user=$HOME
+# update&upgrade
+sudo apt update -y && sudo apt upgrade -y
+# stamp datetime update
+cat << EOF > $setting/update.json
+{
+  "version": "$(curl -s https://raw.githubusercontent.com/tonmai0705/innit-OrangePi-Loom-Update/refs/heads/Develop/version.txt)",
+  "date": "$(date +%Y%m%d)"
+}
